5283279b4bb6963a7620ff4aadfda5331f19913f
nurcahya2442 <nurcahya2443@gmail.com>
2019-08-08 12:48:02 +0700

    PWPB
    Belajar Intent

---
+++ app/src/main/java/com/example/belajar_intent/IntentActivity.java
@@ -0,0 +1,13 @@
+package com.example.belajar_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
